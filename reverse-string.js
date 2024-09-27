4129fb4c3d79a0f70d47418acf19ab7153c3c41c
Rakesh Kumar <r.nimriya@gmail.com>
2024-09-27 10:50:38 +0530

    reverse String

---
+++ reverse-string.js
@@ -0,0 +1,21 @@
+function reverseString(str) 
+{
+    let reversed = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversed += str[i];
+    }
+    return reversed;
+}    
+
+console.log(reverseString("Rakesh Kumar"))
+
+
+// 2nd Example
+// ===============================
+
+
+function reverseStr(str)
+{
+  return str.split("").reverse().join("");
+}
+console.log(reverseStr("Manoj Kumar"))
